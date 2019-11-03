ca752fd6966369bb23f24c25e38bb68871840447
Michał Januszewski <blackrainpl@gmail.com>
2019-11-03 20:11:11 +0100

    Fixed compilation.

--- Package.swift
+++ Package.swift
@@ -1,3 +1,5 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
@@ -14,7 +16,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MessageKit"
+            name: "MessageKit",
             path: "Sources"
         )
     ]
